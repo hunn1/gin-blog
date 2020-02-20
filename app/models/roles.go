f85b436e9c931b2f76cf4c66719bc7bf0ed50548
lookwi <lookwi@163.com>
2020-02-20 15:00:47 +0800

    :sparkles: RBAC

--- app/models/roles.go
+++ app/models/roles.go
@@ -1,6 +1,9 @@
 package models
 
-import "Kronos/library/databases"
+import (
+	"Kronos/library/databases"
+	"github.com/jinzhu/gorm"
+)
 
 // 角色
 type Roles struct {
@@ -32,10 +35,65 @@ func GetCount(maps interface{}) (int, error) {
 	return count, nil
 }
 
-//func GetRolesPage(pageNum int, pageSize int, maps interface{}) ([]*Roles, error) {
-//
-//	return
-//}
+func GetRolesPage(pageNum int, pageSize int, maps interface{}) ([]*Roles, error) {
+	var role []*Roles
+	err := databases.DB.Preload("Permissions").Where(maps).Offset(pageNum).Limit(pageSize).Find(&role).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return role, nil
+}
+
+func GetRoleByID(id int) (*Roles, error) {
+	var role Roles
+	err := databases.DB.Preload("Permissions").Where("id = ?", id).First(&role).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return &role, nil
+}
+
+func CheckRoleName(name string) (bool, error) {
+	var role Roles
+	err := databases.DB.Where("title=?", name).First(&role).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return false, nil
+	}
+	if role.ID > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+func EditRole(id int, data map[string]interface{}) error {
+	var role []Roles
+	var permsiss Permissions
+	databases.DB.Where("id in (?)", data["permissions_id"].(int)).Find(&permsiss)
+
+	err := databases.DB.Where("id = ?", id).Find(&role).Error
+	if err != nil {
+		return err
+	}
+	databases.DB.Model(&role).Association("Permissions").Replace(permsiss)
+	databases.DB.Model(&role).Update(data)
+	return nil
+}
+
+func AddRole(data map[string]interface{}) (id int, err error) {
+	role := Roles{
+		Title:       data["title"].(string),
+		Description: data["description"].(string),
+	}
+	var per Permissions
+	databases.DB.Where("id in (?)", data["permissions_id"].(int)).Find(&per)
+	err = databases.DB.Create(&role).Association("Permissions").Append(&per).Error
+	if err != nil {
+		return 0, err
+	}
+
+	return int(role.ID), nil
+
+}
 
 //func (r *Roles) Add() (id int, err error) {
 //	role := map[string]interface{}{
