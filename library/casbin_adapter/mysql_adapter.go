4bf0bfc3e8bee4f1bbd3a1358ea17d2ff7003d05
lookwi <lookwi@163.com>
2020-02-27 10:48:32 +0800

    :sparkles: RBAC

--- library/casbin_adapter/mysql_adapter.go
+++ library/casbin_adapter/mysql_adapter.go
@@ -1,6 +1,7 @@
 package casbin_adapter
 
 import (
+	"Kronos/library/databases"
 	"fmt"
 	"github.com/casbin/casbin/v2"
 	adapter "github.com/casbin/gorm-adapter/v2"
@@ -11,22 +12,10 @@ import (
 // 初始化权限 数据库适配器
 func InitAdapter() (*casbin.SyncedEnforcer, error) {
 
-	host := viper.GetString("db.host")
-	user := viper.GetString("db.user")
-	pass := viper.GetString("db.pass")
-	dbname := viper.GetString("db.dbname")
-	charset := viper.GetString("db.charset")
-	loc := viper.GetString("db.loc")
-	native := viper.GetString("db.native")
-
-	dabs := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=True&loc=%s&allowNativePasswords=%s",
-		user, pass, host,
-		dbname, charset, loc,
-		native,
-	)
-
-	//a, err := adapter.NewAdapterByDB(databases.DB)
-	a, err := adapter.NewAdapter("mysql", dabs, true)
+	if databases.DB == nil {
+		databases.InitDB()
+	}
+	a, err := adapter.NewAdapterByDBUsePrefix(databases.DB, viper.GetString("db.prefix"))
 	if err != nil {
 		return nil, fmt.Errorf("can not Init: %v", err.Error())
 	}
